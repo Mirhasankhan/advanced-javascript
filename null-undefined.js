cbf5057e682b0ba6698605c06166092112724c46
mirhasankhan <mirhasan000034@gmail.com>
2022-07-01 21:44:21 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,21 @@
+// if you don't set the value of a variable than it will show as undefined
+// let num;
+
+// if you don't return form a function than it will show as undefined
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// if you give the value of one parameter but didn't give value of another's than it will show as undefined.
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+
+// if there is no object property is there than it will show as undefined
+// const lover = {name: 'murad', phone: 45387};
+// console.log(lover.gf);
+
+let fun = undefined;
+console.log(fun);
+
