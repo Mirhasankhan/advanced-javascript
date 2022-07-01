2e1a9588c8781c0d619f912d581602d633949c34
mirhasankhan <mirhasan000034@gmail.com>
2022-07-01 18:49:19 +0600

    true vs false

---
+++ truthyFalsy.js
@@ -0,0 +1,21 @@
+const number = 5;
+
+if(number > 10){
+    console.log('it is true');
+}
+else{
+    console.log('it is false');
+}
+
+// // true
+// if it a number than it true
+// if it a string with property than it is true
+// empty array is true
+// empty object will be true
+
+// false
+// if it is number with 0 value it is false
+// if it is a empty string than it is false
+// if it is undefined than it is false
+// if it is null than it is false
+// if it is NaN than it is false
